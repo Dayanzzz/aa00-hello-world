728d8e846c802477d17528d6fdd96c493abdab81
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-06-18 15:37:24 -0700

    testing1

---
+++ problems/02-code-comments.js
@@ -0,0 +1,28 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log('Welcome to App Academy!');
+
+/* Print more messages to the terminal using console.log */
+
+// 2. This is a single line comment.
+// console.log('This is will not be printed to the terminal')
+
+
+//WHY WONT IT ACCEPT MY SINGLE LINE COMMENT 
+
+
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+/* this
+is
+my
+multi
+*/
+// Your code here 
